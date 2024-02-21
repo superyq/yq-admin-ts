bcff4621dcc1fbc88e0668c7018f7b38fb1ad114
yqcoder <447845126@qq.com>
2024-02-21 15:46:09 +0800

    chore: init

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,17 +1,13 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(({ command }) => {
   if (command == "serve") {
-    // 'server' 开发环境
-    console.log("开发");
+    // 'server' 开发环境 TODO
   } else {
-    // 'build' 生产环境
-    console.log("生产");
+    // 'build' 生产环境 TODO
   }
 
   return {
@@ -25,11 +21,10 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
     },
     server: {
       host: "127.0.0.1",
-      port: "8080",
+      port: 8080,
       strictPort: true,
       open: true,
       proxy: {
-        "/foo": "http://localhost:1111",
         "/api": {
           target: "http://localhost:1112",
           changeOrigin: true,
