24d6abffb4f00a64af902e7a2f626223fd917cb1
yqcoder <447845126@qq.com>
2024-02-26 16:02:50 +0800

    chore: init

--- admin/src/utils/permission.ts
+++ admin/src/utils/permission.ts
@@ -1,2 +1,38 @@
 // 字符权限校验
-export const checkPermi = (value) => {};
+export const checkPermi = (value: string[]): boolean => {
+  if (value && value instanceof Array && value.length > 0) {
+    const permissions = ["system:user:add"];
+    const permissionDatas = value;
+    const all_permission = "*:*:*";
+
+    const hasPermission = permissions.some(
+      (permission) =>
+        all_permission === permission || permissionDatas.includes(permission)
+    );
+
+    return hasPermission;
+  } else {
+    console.error(
+      `need roles! Like checkPermi="['system:user:add','system:user:edit']"`
+    );
+    return false;
+  }
+};
+
+// 角色权限校验
+export const cehckRole = (value: string[]): boolean => {
+  if (value && value instanceof Array && value.length > 0) {
+    const roles = ["admin", "editor"];
+    const permissionRoles = value;
+    const super_admin = "admin";
+
+    const hasRole = roles.some(
+      (role) => super_admin === role || permissionRoles.includes(role)
+    );
+
+    return hasRole;
+  } else {
+    console.error(`need roles! Like checkRole="['admin','editor']"`);
+    return false;
+  }
+};
