75f37f0e7c6edcb64574d75c6a96adf815519117
yqcoder <447845126@qq.com>
2024-03-11 00:32:04 +0800

    chore: init

--- admin/src/pages/login/index.vue
+++ admin/src/pages/login/index.vue
@@ -1,10 +1,28 @@
-<script setup lang="ts"></script>
+<script setup>
+import CopeRight from "@/components/CopyRight.vue";
+import LoginForm from "./LoginForm.vue";
+</script>
 
 <template>
-  <div class="p-login">
-    <div><button v-hasPermi="['system:user:add']">asd</button></div>
-    <div><button v-hasPermi="['system:user:edit']">qqq</button></div>
+  <div class="login-bg u__flex-c">
+    <LoginForm />
+    <CopeRight />
   </div>
 </template>
 
-<style lang="scss" scope></style>
+<style lang="scss">
+.login__box .n-form-item {
+  margin-top: -20px;
+}
+</style>
+
+<style lang="scss" scoped>
+.login-bg {
+  width: 100vw;
+  height: 100vh;
+  background-image: url("@/assets/imgs/login_bg.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  overflow: hidden;
+}
+</style>
