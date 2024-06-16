9a4e31cd1687715bdb0923cfd6256c1092b435cb
yqcoder <447845126@qq.com>
2024-06-16 22:16:05 +0800

    chore: init

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -67,6 +67,33 @@ export default defineConfig(({ command }) => {
       emptyOutDir: true,
       rollupOptions: {
         external: ["@amap/amap-jsapi-loader"],
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vender";
+          }
+        },
+        output: {
+          entryFileNames: "js/[name]-[hash].js",
+          chunkFileNames: "js/[name]-[hash].js",
+          assetFileNames(assetInfo) {
+            if (assetInfo.name.endsWith(".css")) {
+              return "css/[name]-[hash].css";
+            }
+            const imgExts = [
+              ".png",
+              ".jpg",
+              ".jpeg",
+              ".webp",
+              ".svg",
+              ".gif",
+              ".icon",
+            ];
+            if (imgExts.some((ext) => assetInfo.name.endsWith(ext))) {
+              return "imgs/[name]-[hash].[ext]";
+            }
+            return "assets/[name]-[hash].[ext]";
+          },
+        },
       },
     },
   };
